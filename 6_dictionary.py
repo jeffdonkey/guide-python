1370ce04e5b764b1ae403f9af1ecb405b22759cf
jeffdonkey <8jefa8@gmail.com>
2023-01-23 11:30:13 -0500

    updates

---
+++ 6_dictionary.py
@@ -0,0 +1,14 @@
+# dictionary
+
+my_dict = {
+    "animal": "dog",
+    "name": "Lottie Dottie",
+    "age": 6,
+    "like_to_party": True
+}
+
+    # enclosed in {}
+    # uses key:value pairs
+    # pairs seperated by commas
+    # values can be of any type
+
